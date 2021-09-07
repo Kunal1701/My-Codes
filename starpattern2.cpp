5552cbc59fd1d768f6c7a596fce20de9fee9a351
Kunal1701 <80157692+Kunal1701@users.noreply.github.com>
2021-09-07 21:07:19 +0530

    M  starpattern

---
+++ starpattern2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int j = 1; j <=4- i; j++)
+        {
+            cout << "*";
+        }
+        int space=2*i-2;
+        for (int k = 1; k <=space; k++)
+        {
+            cout<<" ";
+        }
+        for (int j = 1; j <= 4-i; j++)
+        {
+            cout << "*";
+        }
+        
+
+        cout<<endl;
+        
+    }
+
+    return 0;
+}
