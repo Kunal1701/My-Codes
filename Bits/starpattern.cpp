b0efc9ce42132013d3abddc89fb99f31db094212
Kunal1701 <80157692+Kunal1701@users.noreply.github.com>
2021-09-07 20:57:40 +0530

    pattern

---
+++ Bits/starpattern.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        int space=8-2*i;
+        for (int k = 1; k <=space; k++)
+        {
+            cout<<" ";
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        
+
+        cout<<endl;
+        
+    }
+
+    return 0;
+}
