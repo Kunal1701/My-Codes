3140e13d8e5e7e07ea0d3867b66f412e5d5209a3
Kunal1701 <80157692+Kunal1701@users.noreply.github.com>
2021-09-09 10:28:56 +0530

    9 `Sept`

---
+++ Linked List/circular_list.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+struct node
+{
+    int data;
+    node*link;
+};
+node*tail;
+node*add_to_empty(int data){
+    node*temp=new node();
+    temp->data=data;
+    temp->link=temp;
+    return temp;
+}
+node*add_to_beg(node*tail,int data){
+    node*newp=new node();
+    newp->data=data;
+    newp->link=tail->link;
+    tail->link=newp;
+    return tail;
+}
+void print(node*tail){
+    node*ptr=tail->link;
+   do
+   {
+       cout<<ptr->data<<" ";
+       ptr=ptr->link;
+   } while (ptr!=tail->link);
+   
+}
+
+
+
+int main(){
+    tail=add_to_empty(4);
+    tail=add_to_beg(tail,3);
+    tail=add_to_beg(tail,2);
+    tail=add_to_beg(tail,1);
+    print(tail);
+    cout<<endl;
+
+return 0;
+}
